108f91992315809dca0c686a77b4aa55107d4278
Soskretkov <oskrserg@gmail.com>
2023-09-11 19:19:40 +0300

    переделан JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
 
-  return ( 
-    // весь JSX-код является декларативным (начало)
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Домашка на Vite + React + TS</h1>
-      <div className="card">
-        <button>It is currently the year: {currentYear}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-    // весь JSX-код является декларативным (конец)
-  )
+  return [
+    React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'a',
+        { href: 'https://vitejs.dev', target: '_blank' },
+        React.createElement('img', {
+          src: viteLogo,
+          className: 'logo',
+          alt: 'Vite logo'
+        })
+      ),
+      React.createElement(
+        'a',
+        { href: 'https://react.dev', target: '_blank' },
+        React.createElement('img', {
+          src: reactLogo,
+          className: 'logo react',
+          alt: 'React logo'
+        })
+      )
+    ),
+    React.createElement(
+      'h1',
+      null,
+      'Домашка на Vite + React + TS'
+    ),
+    React.createElement(
+      'div',
+      { className: 'card' },
+      React.createElement(
+        'button',
+        null,
+        `It is currently the year: ${currentYear}`
+      ),
+      React.createElement(
+        'p',
+        null,
+        'Edit ',
+        React.createElement('code', null, 'src/App.tsx'),
+        ' and save to test HMR'
+      )
+    ),
+    React.createElement(
+      'p',
+      { className: 'read-the-docs' },
+      'Click on the Vite and React logos to learn more'
+    )
+  ];
 }
 
-export default App
+export default App;
