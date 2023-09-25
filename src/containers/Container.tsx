a74bfb046ba367d07dcf1ba5df1b56e931b191c1
Soskretkov <oskrserg@gmail.com>
2023-09-25 23:24:44 +0300

    Container.tsx: класс ticTacToeBoard

--- src/containers/Container.tsx
+++ src/containers/Container.tsx
@@ -7,67 +7,57 @@ import styles from './Container.module.scss';
 
 const PLAYER_NAMES = ['X', 'O'] as const;
 
-const INITIAL_MATRIX_VALUES = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-] as const;
-
 type CellValue = typeof PLAYER_NAMES[number] | null;
-type CellMatrix = Array<Array<CellValue>>;
 
-export default function Container() {
-  // создание экземпляра класса PlayerManager
-  const playerManagerRef = useRef(new PlayerManager(PLAYER_NAMES)).current;
+export default function Container(): React.JSX.Element {
+  // создание экземпляров классов PlayerManager и TicTacToeBoard
+  const playerManager = useRef(new PlayerManager(PLAYER_NAMES)).current;
+  const ticTacToeBoard = useRef(new TicTacToeBoard()).current;
 
-  const [strPlayerSymbol, setPlayerSymbol] = useState(playerManagerRef.togglePlayerAndGetName());
+  const [strPlayerSymbol, setPlayerSymbol] = useState(playerManager.togglePlayerAndGetName());
   const [isGameActive, setIsGameActive] = useState(true);
-
-  // константа INITIAL_MATRIX_VALUES существует только для инициализации, а потому клонируем ее как новый объект, а не передаем саму
-  // (делать эту константу переменной не выход, захватывать и менять внешнее окружение из функции плохая практика в функ. программировании)
-  const [cellMatrixValues, setCellMatrixValues] = useState<CellValue[][]>(INITIAL_MATRIX_VALUES.map(row => [...row]));
+  const [cellMatrixValues, setCellMatrixValues] = useState(ticTacToeBoard.board);
 
 
-  function cellClickHandler(uintX: number, uintY: number) {
-    // выход или игра завершена или случился клик в ячейку, где состояние уже есть
+  function cellClickHandler(uintX: number, uintY: number): void {
+    // не требуются изменения если игра завершена или произошел клик в ячейку, где значение уже есть
     if (!isGameActive || cellMatrixValues[uintY][uintX] !== null) {
       return;
     }
 
-    // создание новой копии матрицы
-    const newCellMatrixValues = cellMatrixValues.map(row => [...row]);
+    // изменение отдельного значения в матрице
+    ticTacToeBoard.setBoardCell(uintY, uintX, strPlayerSymbol as CellValue);
 
-    // изменение значения в копии матрицы
-    newCellMatrixValues[uintY][uintX] = strPlayerSymbol as CellValue;
+    // чтобы избежать только мутации избегаем прямой передачи ticTacToeBoard.board
+    // и перепаковываем его в новые объекты (хотя и без этого работает)
+    const newBoard = ticTacToeBoard.board.map(row => [...row]);
+    setCellMatrixValues(newBoard);
 
-    // установка нового состояния на основе измененной матрицы
-    setCellMatrixValues(newCellMatrixValues);
-
-    // проверка условий победы
-    const isWinCondition = checkWinCondition(newCellMatrixValues);
-    if (isWinCondition) {
+    // обработка победы
+    if (ticTacToeBoard.checkWinCondition()) {
       setIsGameActive(false);
       return;
     }
 
     // смена текущего игрока
-    const nextPlayer = playerManagerRef.togglePlayerAndGetName();
+    const nextPlayer = playerManager.togglePlayerAndGetName();
     setPlayerSymbol(nextPlayer);
   }
 
 
-  function resetGame() {
-    playerManagerRef.resetByDefault();
+  function resetGame(): void {
+    // обнуление очерди ходов и игровой доски
+    playerManager.resetByDefault();
+    ticTacToeBoard.resetAllBoardValues();
+
     // сброс значений в клетках
-    setCellMatrixValues(INITIAL_MATRIX_VALUES.map(row => [...row]));
+    setCellMatrixValues(ticTacToeBoard.board);
 
     // сброс очереди ходить
-    setPlayerSymbol(playerManagerRef.togglePlayerAndGetName());
+    setPlayerSymbol(playerManager.togglePlayerAndGetName());
 
     // сброс статуса игры на "не завершена"
-    if (!isGameActive) {
-      setIsGameActive(true);
-    }
+    setIsGameActive(true);
   }
 
 
@@ -81,7 +71,56 @@ export default function Container() {
 }
 
 
-// принимает массив имен игроков, отсортированный в порядке очереди ходить, призван следить за очередью хода
+class TicTacToeBoard {
+  board: CellValue[][];
+
+  constructor() {
+    this.board = [];
+    this.resetAllBoardValues();
+  }
+
+  resetAllBoardValues(): void {
+    this.board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+  }
+
+  setBoardCell(uintY: number, uintX: number, strPlayerSymbol: CellValue): void {
+    this.board[uintY][uintX] = strPlayerSymbol;
+  }
+
+  checkWinCondition(): boolean {
+    const board = this.board;
+    // проверка строк
+    for (let i = 0; i < 3; i++) {
+      if (board[i][0] !== null && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+        return true;
+      }
+    }
+
+    // проверка столбцов
+    for (let i = 0; i < 3; i++) {
+      if (board[0][i] !== null && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+        return true;
+      }
+    }
+
+    // проверка диагоналей
+    if (board[0][0] !== null && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+      return true;
+    }
+    if (board[0][2] !== null && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+      return true;
+    }
+
+    return false;
+  }
+}
+
+
+// принимает массив имен игроков, отсортированный в порядке очереди ходов, призван следить за очередью хода
 class PlayerManager {
   playerNames: readonly string[];
   #uintMaxPlayerIndex: number;
@@ -93,46 +132,19 @@ class PlayerManager {
   }
 
   // передать очередь хода
-  togglePlayerAndGetName() {
+  togglePlayerAndGetName(): string {
     const strActivePlayerName = this.getActivePlayerName()
     this.#uintCurrentPlayerIndex = (this.#uintCurrentPlayerIndex + 1) % this.#uintMaxPlayerIndex;
     return strActivePlayerName;
   }
 
   // сбросить очередь ходов на первоначальные
-  resetByDefault() {
+  resetByDefault(): void {
     this.#uintCurrentPlayerIndex = 0;
   }
 
   // имя текущего игрока
-  getActivePlayerName() {
+  getActivePlayerName(): string {
     return this.playerNames[this.#uintCurrentPlayerIndex]
   }
-}
-
-
-function checkWinCondition(board: CellMatrix): boolean {
-  // Проверка строк
-  for (let i = 0; i < 3; i++) {
-    if (board[i][0] !== null && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
-      return true;
-    }
-  }
-
-  // Проверка столбцов
-  for (let i = 0; i < 3; i++) {
-    if (board[0][i] !== null && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
-      return true;
-    }
-  }
-
-  // Проверка диагоналей
-  if (board[0][0] !== null && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-    return true;
-  }
-  if (board[0][2] !== null && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-    return true;
-  }
-
-  return false;
 }
