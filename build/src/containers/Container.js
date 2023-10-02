fa4748e5261c9fdbe29265eb26e4c64534ea9b9e
Soskretkov <oskrserg@gmail.com>
2023-10-02 11:26:20 +0300

    готово

--- build/src/containers/Container.js
+++ build/src/containers/Container.js
@@ -1,42 +1,12 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importStar(require("react"));
+const react_1 = __importDefault(require("react"));
+const Form_1 = __importDefault(require("../components/Form"));
 function Container() {
-    const [count, setCount] = (0, react_1.useState)(0);
-    return (react_1.default.createElement(react_1.default.Fragment, null,
-        react_1.default.createElement("h1", null, "Vite + React + TS"),
-        react_1.default.createElement("div", { className: "card" },
-            react_1.default.createElement("button", { onClick: () => setCount((count) => count + 1) },
-                "count is ",
-                count),
-            react_1.default.createElement("p", null,
-                "Edit ",
-                react_1.default.createElement("code", null, "src/App.tsx"),
-                " and save to test HMR")),
-        react_1.default.createElement("p", { className: "read-the-docs" }, "Click on the Vite and React logos to learn more")));
+    return (react_1.default.createElement(Form_1.default, null));
 }
 exports.default = Container;
 //# sourceMappingURL=Container.js.map
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,89 @@
 import React, { useState, useRef } from 'react';
 import styles from './Form.module.scss';
 
-// в порядке эксперимента класс FormData предполагается одновременно использоваться и как "тип" и как "хук"
-// мотивация: хук не может разместить в себе type, а хук + внешний type !== консистентноcть
+
+// Этот модуль имеет экспериментальный характер.
+// хотелось определить тип и его методы в одном объекте и доказать жизнеспособность такого подхода.
+// В отличие от хуков, которые не могут напрямую определять типы пропсов,
+// и требуют использование дополнительного внешнего типа, с нарушением консистентности,
+// класс FormData может предложить элегантное решение этой проблемы:
+// function Form({ formData }: { formData: FormData }) {}
+
+
+export default function CreateForm() {
+  const formData = new FormData();
+  return (
+    <Form formData={formData} />
+  );
+}
+
+
+function Form({ formData }: { formData: FormData }) {
+  const formDataRef = useRef(formData).current;
+
+  // определение ref для кнопки отправки
+  const submitButtonRef = useRef<HTMLButtonElement | null>(null);
+
+  // useState интересен только как способ рендерить
+  const [, forceRender] = useState({});
+
+
+  function onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+
+    formDataRef
+      .setField(name, value)
+      .validate();
+
+    if (formDataRef.isValid()) {
+      // перемещение фокуса на кнопку
+      submitButtonRef.current && submitButtonRef.current.focus();
+    }
+
+    // перерендерит компонент
+    forceRender({});
+  }
+
+
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (!formDataRef.isValid()) {
+      return; // прервать отправку формы, если она невалидна
+    }
+    formDataRef.sendFormData();
+  }
+
+  // стиль для "неактивной" кнопки (disabled прямо в теге не рабочая идея - из-за него слетит фокус)
+  const buttonStyle = formDataRef.isValid() ? styles['button--activeButton'] : styles['button--inactiveButton'];
+
+  return (
+    <form className={styles.form} onSubmit={onSubmit}>
+      <input name="email"
+        type="email"
+        placeholder="Почта"
+        // валидация поля email намеренно происходит только при потере фокуса
+        onBlur={onChange}
+      />
+      <div className={styles.error}>{formDataRef.errors.email}</div>
+      <input name="password"
+        type="password"
+        placeholder="Пароль"
+        onChange={onChange}
+      />
+      <div className={styles.error}>{formDataRef.errors.password}</div>
+      <input name="confirmPassword"
+        type="password"
+        placeholder="Повторите пароль"
+        onChange={onChange}
+      />
+      <div className={styles.error}>{formDataRef.errors.confirmPassword}</div>
+      <button ref={submitButtonRef} className={buttonStyle} type="submit">Зарегистрироваться</button>
+
+    </form>
+  )
+}
+
+
 class FormData {
   values: { [key: string]: string } = {
     email: '',
@@ -50,16 +131,20 @@ class FormData {
   validate = (): this => {
     const emailText = this.values.email;
     let strNewEmailErr = '';
+    let strNewPasswordErr = '';
     let strNewConfirmPasswordErr = '';
 
-    if (!/^[\w_@]*$/.test(emailText)) {
-      strNewEmailErr = 'Неверный логин. Допустимые символы: буквы, цифры, нижнее подчёркивание и @';
-    } else if (emailText.length > 20) {
-      strNewEmailErr = 'Неверный логин. Должно быть не больше 20 символов';
+    // требования минимальны: @ и один символ слева и справа
+    const emailRegex = /^.+@.+$/;
+    if (!emailRegex.test(emailText)) {
+      strNewEmailErr = 'Неверный формат email';
     }
 
+    if (this.values.password.length <= 4) {
+      strNewPasswordErr = 'Пароль должен содержать более 4 символов';
+    }
 
-    if (this.values.password) {
+    if (this.values.confirmPassword) {
       if (this.values.password !== this.values.confirmPassword) {
         strNewConfirmPasswordErr = 'Пароли не совпадают';
       }
@@ -67,76 +152,15 @@ class FormData {
 
     Object.assign(this.errors, {
       email: strNewEmailErr,
+      password: strNewPasswordErr,
       confirmPassword: strNewConfirmPasswordErr,
     });
 
     return this;  // возвращаем this для возможности создания цепочек вызовов
   }
 
+
   sendFormData = () => {
     console.log(this.values);
   };
-}
-
-
-export default function Form() {
-  // создаем экземпляр класса
-  const formData = new FormData();
-  const formDataRef = useRef(formData).current;
-
-  // Определение ref для кнопки отправки
-  const submitButtonRef = useRef<HTMLButtonElement | null>(null);
-
-  // кажется, мой код является уникальным подходом к управлению данными формы 
-  // useState интересен только как способ рендерить, но хранит все екземпляр класса
-  const [, forceRender] = useState(null);
-  // const [loginError, setLoginError] = useState(null);
-
-
-  function onChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const { name, value } = event.target;
-    formDataRef
-      .setField(name, value)
-      .validate();
-      console.log(formDataRef.errors);  // добавлено для отладки
-      // console.log(formDataRef.values);  // добавлено для отладки
-
-    forceRender(null);
-  }
-
-
-  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    if (formDataRef.isValid()) {
-      formDataRef.sendFormData();
-      // Перемещение фокуса на кнопку
-      submitButtonRef.current && submitButtonRef.current.focus();
-    } else {
-      forceRender(null);  // Заставить компонент перерендериться, чтобы отобразить ошибки
-    }
-  }
-
-  return (
-    <form className={styles.form} onSubmit={onSubmit}>
-      <input name="email"
-        type="email"
-        placeholder="Почта"
-        onChange={onChange}
-      />
-      <div className={styles.error}>{formDataRef.errors.email}</div>
-      <input name="password"
-        type="password"
-        placeholder="Пароль"
-        onChange={onChange}
-      />
-      <div className={styles.error}>{formDataRef.errors.password}</div>
-      <input name="confirmPassword"
-        type="password"
-        placeholder="Повторите пароль"
-        onChange={onChange}
-      />
-      <div className={styles.error}>{formDataRef.errors.confirmPassword}</div>
-      <button ref={submitButtonRef} disabled={!formDataRef.isValid()} type="submit">Зарегистрироваться</button>
-    </form>
-  )
 }
