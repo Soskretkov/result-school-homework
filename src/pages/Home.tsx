c609c9b7bf2799627fdb29766913702d0dd7e3cd
Soskretkov <oskrserg@gmail.com>
2023-10-05 14:47:27 +0300

    Home.tsx refactor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,36 @@
 // eslint-disable-next-line
 import React, { useState } from 'react';
 import BasePage from './Base';
-import HookFormPage from './Base';
+import HookFormPage from './HookForm';
 import styles from './Home.module.scss';
 
 
 export default function HomePage() {
-  // варианты перехода на другие страницы
-  enum SubPageEnum {
+  // доступные вебстраницы
+  enum WebPagesEnum {
+    HOME,
     BASE,
     HOOK_FORM,
   }
 
-  const [activePage, setActivePage] = React.useState<SubPageEnum | null>(null);
+  const [activePage, setActivePage] = React.useState(WebPagesEnum.HOME);
 
   switch (activePage) {
-    case SubPageEnum.BASE:
+    case WebPagesEnum.BASE:
       return (
         <BasePage />
       );
-    case SubPageEnum.HOOK_FORM:
+    case WebPagesEnum.HOOK_FORM:
       return (
         <HookFormPage />
       );
+    case WebPagesEnum.HOME:
     default:
       return (
         <div className={styles.home}>
           < h1 > Выберите: </h1 >
-          <button onClick={(_) => setActivePage(SubPageEnum.BASE)}>Первая часть задания на голом React</button>
-          <button onClick={(_) => setActivePage(SubPageEnum.HOOK_FORM)}>Вторая часть задания на React Hook Form и Yup</button>
+          <button onClick={(_) => setActivePage(WebPagesEnum.BASE)}>Первая часть задания на голом React</button>
+          <button onClick={(_) => setActivePage(WebPagesEnum.HOOK_FORM)}>Вторая часть задания на React Hook Form и Yup</button>
         </div>
       );
   }
